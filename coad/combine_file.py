1c7b6343c2fb20661322a5e3c1fc9e2f1877a568
Wonyoung Cho <bourbaki10@gmail.com>
2022-03-07 21:26:59 +0900

    Create combine_file.py

---
+++ coad/combine_file.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+fname = 'beta_values_nonna'
+
+df1 = pd.read_parquet(fname+'0.par')
+df2 = pd.read_parquet(fname+'1.par')
+df3 = pd.read_parquet(fname+'2.par')
+df4 = pd.read_parquet(fname+'3.par')
+df5 = pd.read_parquet(fname+'4.par')
+df6 = pd.read_parquet(fname+'5.par')
+
+df = pd.concat([df1,df2,df3,df4,df5,df6],axis=1)
+
+df.to_csv(fname+'.tsv', sep='\t')
