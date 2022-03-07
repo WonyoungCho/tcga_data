e28f696b05f27e23c79d0c63c5c6c5156d524524
Wonyoung Cho <bourbaki10@gmail.com>
2022-03-07 21:52:11 +0900

    Update download.sh

--- kich/download.sh
+++ kich/download.sh
@@ -1,6 +1,9 @@
 wget https://github.com/WonyoungCho/tcga_data/raw/main/kich/combine_files.py
 wget https://github.com/WonyoungCho/tcga_data/raw/main/kich/beta_values_nonna1.par
 wget https://github.com/WonyoungCho/tcga_data/raw/main/kich/beta_values_nonna2.par
+wget https://github.com/WonyoungCho/tcga_data/raw/main/kich/TCGA-KICH_HTSeq_2.phe
+wget https://github.com/WonyoungCho/tcga_data/raw/main/kich/TCGA-KICH_HTSeq_FPKM.tsv.par
+wget https://github.com/WonyoungCho/tcga_data/raw/main/kich/TCGA-KICH_HTSeq_FPKM-UQ.tsv.par
 
 python3 combine_files.py
 
