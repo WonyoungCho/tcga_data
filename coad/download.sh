1ca06ed2934c7d1df5d18e857de04fbbb2d84108
Wonyoung Cho <bourbaki10@gmail.com>
2022-03-07 21:26:33 +0900

    Update download.sh

--- coad/download.sh
+++ coad/download.sh
@@ -1,6 +1,8 @@
-wget 
-wget 
-wget 
-wget 
-wget 
-wget 
+wget https://github.com/WonyoungCho/tcga_data/raw/main/coad/beta_values_nonna0.par
+wget https://github.com/WonyoungCho/tcga_data/raw/main/coad/beta_values_nonna1.par
+wget https://github.com/WonyoungCho/tcga_data/raw/main/coad/beta_values_nonna2.par
+wget https://github.com/WonyoungCho/tcga_data/raw/main/coad/beta_values_nonna3.par
+wget https://github.com/WonyoungCho/tcga_data/raw/main/coad/beta_values_nonna4.par
+wget https://github.com/WonyoungCho/tcga_data/raw/main/coad/beta_values_nonna5.par
+
+python3 combine_files.py
